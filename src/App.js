431545eb3a6cfd17b98955abd9463d31a9c64d89
MHines10 <cd.mhines@gmail.com>
2023-01-23 16:47:26 -0500

    Move state from out of each button to the App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Button from "./components/Button"
+import {useState} from "react";
+import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
 
-function App(props) {
+function App() {
     let myName = 'Monti'
     let buttons = [
         {color: 'primary', step: 1},
@@ -10,13 +11,20 @@ function App(props) {
         {color: 'success', step: 100},
         {color: 'danger', step: 1000},
     ]
+
+    const [count, setCount] = useState(0);
+
+    function handleClick(step){
+      setCount(count + step)
+    };
+
     return (
         <>
             <Navbar name = {myName} city = "Newport News"/>
             <div className="container">
-                <h1>Hello {myName}</h1>
+                <h1>Hello {myName}, Count {count}</h1>
                 {buttons.map((button, idx) => <Button color={button.color}
-                step={button.step} key={idx}/>)}
+                step={button.step} key={idx} handleClick={handleClick}/>)}
             </div>
         </>
         );
